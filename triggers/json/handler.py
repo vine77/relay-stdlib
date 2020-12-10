c450f0ae607400a8c790ec1def2fa5b599f9fb56
Kenaz <kenaz@puppetlabs.com>
2020-12-10 11:21:23 -0800

    Updating logs to output webhook payload

--- triggers/json/handler.py
+++ triggers/json/handler.py
@@ -2,18 +2,19 @@ from relay_sdk import Interface, WebhookServer
 from quart import Quart, request
 
 import logging
+from pprint import pformat
 
 relay = Interface()
 app = Quart('json')
 
 logging.getLogger().setLevel(logging.INFO)
 
-
 @app.route('/', methods=['POST'])
 async def handler():
-    relay.events.emit(await request.get_json(force=True))
+    r = await request.get_json(force=True)
+    logging.info("Received the following payload: \n%s", pformat(r))
+    relay.events.emit(r)
     return {}, 202, {}
-
 
 if __name__ == '__main__':
     WebhookServer(app).serve_forever()
